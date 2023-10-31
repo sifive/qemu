add4c91dbd01e3da83eaa120cc84130954bb4a39
Deepak Gupta <debug@rivosinc.com>
2023-10-31 16:12:30 -0700

    linux-user/riscv: qemu-user support for riscv cfi related prctls
    To enable qemu-user for riscv to run user apps compiled with cfi, prctl support
    in qemu is needed. This patch provides that support. May need to re-factor in
    future.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>

--- linux-user/riscv/target_prctl.h
+++ linux-user/riscv/target_prctl.h
@@ -1,1 +1,28 @@
 /* No special prctl support required. */
+#ifndef RISCV_TARGET_PRCTL_H
+#define RISCV_TARGET_PRCTL_H
+
+static abi_long do_prctl_cfi_set(CPUArchState *env, abi_long flag)
+{
+    if (flag & ~PR_CFI_ALL) {
+        return -TARGET_EINVAL;
+    }
+    if (env->ubcfien && !(flag && PR_CFI_SHADOW_STACK)) {
+        return -TARGET_EACCES;
+    }
+    if (env->ufcfien && !(flag && PR_CFI_LANDING_PAD)) {
+        return -TARGET_EACCES;
+    }
+    if (env_archcpu(env)->cfg.ext_cfi_ss && flag & PR_CFI_SHADOW_STACK) {
+        env->ubcfien = true;
+        return 0;
+    }
+    if (env_archcpu(env)->cfg.ext_cfi_lp && flag & PR_CFI_LANDING_PAD) {
+        env->ufcfien = true;
+        return 0;
+    }
+    return -TARGET_ENOENT;
+}
+#define do_prctl_cfi_set do_prctl_cfi_set
+
+#endif
