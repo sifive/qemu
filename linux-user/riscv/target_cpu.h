4153ebde72cb64cc0f5e320f5bb3ed0c84916c11
Deepak Gupta <debug@rivosinc.com>
2023-12-19 21:52:09 -0800

    riscv/linux-user: parse elf fcfi / bcfi and enable cpu state
    Parse GNU_PROPERTY notes for fcfi and bcfi markers in ELF. If found, enable bcfi
    and fcfi in riscv cpu state for qemu-user on riscv target.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>

--- linux-user/riscv/target_cpu.h
+++ linux-user/riscv/target_cpu.h
@@ -24,4 +24,15 @@ static inline abi_ulong get_sp_from_cpustate(CPURISCVState *state)
 {
    return state->gpr[xSP];
 }
+
+static inline void set_bcfi(CPURISCVState *env)
+{
+   env->ubcfien = true;
+}
+
+static inline void set_fcfi(CPURISCVState *env)
+{
+   env->ufcfien = true;
+}
+
 #endif
