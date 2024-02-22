204d5f79981c43ce22f6ab0f37637032493fcbf3
Jesse Huang <jesse.huang@sifive.com>
2024-02-22 04:42:03 -0800

    Update prctl definitions

--- linux-user/riscv/target_prctl.h
+++ linux-user/riscv/target_prctl.h
@@ -2,34 +2,58 @@
 #ifndef RISCV_TARGET_PRCTL_H
 #define RISCV_TARGET_PRCTL_H
 
-static abi_long do_prctl_cfi_set(CPUArchState *env, abi_long flag)
+/* -TARGET_EINVAL: Unsupported/Invalid flag for this architecture
+ * -TARGET_EACCES: Try to set an already set CFI feature
+ * -TARGET_ENOENT: CFI feature is not supported by CPU
+ **/
+static abi_long do_prctl_cfi_set(CPUArchState *env, abi_long option, abi_long flag)
 {
-    if (flag & ~PR_CFI_ALL) {
-        return -TARGET_EINVAL;
-    }
-    if (env->ubcfien && (flag & PR_CFI_SHADOW_STACK)) {
-        return -TARGET_EACCES;
-    }
-    if (env->ufcfien && (flag & PR_CFI_LANDING_PAD)) {
-        return -TARGET_EACCES;
-    }
-    if (env_archcpu(env)->cfg.ext_cfi_ss && flag & PR_CFI_SHADOW_STACK) {
-        env->ubcfien = true;
-        if (env->ssp == 0) {
-            uintptr_t ssp = 0;
-            ssp = (uintptr_t) mmap(0, TARGET_PAGE_SIZE, PROT_READ | PROT_WRITE,
-                                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-            if ((intptr_t)ssp == -1) {
-                perror("shadow stack alloc");
-                exit(EXIT_FAILURE);
+    if (env_archcpu(env)->cfg.ext_cfi_ss) {
+        switch (option) {
+        case PR_GET_SHADOW_STACK_STATUS:
+            return env->ubcfien;
+        case PR_SET_SHADOW_STACK_STATUS:
+            {
+                if (env->ubcfien)
+                    return -TARGET_EACCES;
+                if (flag & PR_SHADOW_STACK_ENABLE) {
+                    env->ubcfien = true;
+                    if (env->ssp == 0) {
+                        uintptr_t ssp = 0;
+                        ssp = (uintptr_t) mmap(0, TARGET_PAGE_SIZE, PROT_READ | PROT_WRITE,
+                                            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                        if ((intptr_t)ssp == -1) {
+                            perror("shadow stack alloc");
+                            exit(EXIT_FAILURE);
+                        }
+                        env->ssp = ROUND_UP(ssp + 1, TARGET_PAGE_SIZE);
+                    }
+                    return 0;
+                }
+                return -TARGET_EINVAL;
             }
-            env->ssp = ROUND_UP(ssp + 1, TARGET_PAGE_SIZE);
+        case PR_LOCK_SHADOW_STACK_STATUS:
+            return -TARGET_EINVAL;
         }
-        return 0;
     }
-    if (env_archcpu(env)->cfg.ext_cfi_lp && flag & PR_CFI_LANDING_PAD) {
-        env->ufcfien = true;
-        return 0;
+
+    if (env_archcpu(env)->cfg.ext_cfi_lp) {
+        switch (option) {
+        case PR_GET_INDIR_BR_LP_STATUS:
+            return env->ufcfien;
+        case PR_SET_INDIR_BR_LP_STATUS:
+            {
+                if (env->ufcfien)
+                    return -TARGET_EACCES;
+                if (flag & PR_INDIR_BR_LP_ENABLE) {
+                    env->ufcfien = true;
+                    return 0;
+                }
+                return -TARGET_EINVAL;
+            }
+        case PR_LOCK_INDIR_BR_LP_STATUS:
+            return -TARGET_EINVAL;
+        }
     }
     return -TARGET_ENOENT;
 }
